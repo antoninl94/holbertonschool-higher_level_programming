dd00b7f80b1dbb64c0458003e91fa61c4ed0efd6
antoninl94 <9803@holbertonstudents.com>
2025-01-15 14:20:45 +0100

    2-args added condition for 1 argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,13 @@ if __name__ == "__main__":
     argCount = len(sys.argv) - 1
     if len(sys.argv) == 1:
         print(f"{argCount} arguments.")
+    elif len(sys.argv) == 2:
+        print(f"{argCount} argument:")
+        for i, arg in enumerate(sys.argv[1:]):
+            i += 1
+            print(f"{i}: {arg}")
     else:
         print(f"{argCount} arguments:")
-    for i, arg in enumerate(sys.argv[1:]):
-        i += 1
-        print(f"{i}: {arg}")
+        for i, arg in enumerate(sys.argv[1:]):
+            i += 1
+            print(f"{i}: {arg}")
