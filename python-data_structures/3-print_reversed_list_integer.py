c70ac1aa6dfe823414c9c424f62b231e285e0b9c
antoninl94 <9803@holbertonstudents.com>
2025-01-20 10:57:20 +0100

    3-print_reversed_list_integer created

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list)):
+        if i == len(my_list) - 1:
+            break
+        i += 1
+    while i != -1:
+        print("{:d}".format(my_list[i]))
+        i -= 1
