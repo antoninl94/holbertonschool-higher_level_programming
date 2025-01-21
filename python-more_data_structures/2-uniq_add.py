6971c3da6209f40ea227870979358ecc5a2c3596
antoninl94 <9803@holbertonstudents.com>
2025-01-21 16:08:06 +0100

    2-uniq_add created

---
+++ python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    result = 0
+    aux = []
+    for i in my_list:
+        if i not in aux:
+            aux.append(i)
+            result = result + i
+    return result
+        
