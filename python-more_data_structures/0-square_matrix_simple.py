5baac10ae2d9e5961c29993ab6c61733c9fa6080
antoninl94 <9803@holbertonstudents.com>
2025-01-21 11:50:47 +0100

    README and 0-square_matrix_simple created

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def carre(x):
+    return x ** 2
+
+def square_matrix_simple(matrix=[]):
+    result = [list(map(carre, row)) for row in matrix]
+    return result
---
+++ python-more_data_structures/README.md
@@ -0,0 +1,5 @@
+# More Data and Structures
+
+## Synopsis
+
+This directory is about the Datas and Structures of type dictionnary and set
