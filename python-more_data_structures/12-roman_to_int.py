8fc03fc397c0007ddba20f9195ca074d482077f5
antoninl94 <9803@holbertonstudents.com>
2025-01-22 19:03:06 +0100

    12-roman_to_int created

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    result = 0
+    prev = 0
+    if roman_string:
+        for char in reversed(roman_string):
+            if char in roman:
+                    value = roman[char]
+                    if value < prev:
+                         result -= value
+                    else:
+                         result += value
+                         prev = value
+        return result
+    else:
+        return
