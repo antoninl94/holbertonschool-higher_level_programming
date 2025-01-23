52624ad76feccdb773d4d91dea8b48fe18d5df2e
antoninl94 <9803@holbertonstudents.com>
2025-01-23 09:31:27 +0100

    12-roman_to_int added return 0 if roman_string is none or not a string

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if isinstance(roman_string, str) or roman_string is None:
+        return 0
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result = 0
     prev = 0
@@ -13,5 +15,3 @@ def roman_to_int(roman_string):
                     result += value
                     prev = value
         return int(result)
-    else:
-        return 0
