8a6d3d2a69e94df61a20f8ba0168b207820ef000
antoninl94 <9803@holbertonstudents.com>
2025-01-24 14:28:19 +0100

    0-add_integer.py/.txt fixed float

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,11 +12,14 @@ def add_integer(a, b=98):
     Add two numbers and return the sum.
     """
     try:
-        if not isinstance(a, int):
+        if not isinstance(a, int) and not isinstance(a, float):
             raise TypeError("a must be an integer")
-        if not isinstance(b, int):
+        if not isinstance(b, int) and not isinstance(b, float):
             raise TypeError("b must be an integer")
-        #if isinstance(a, float):
-        return a + b
+        if isinstance(a, float):
+            a = int(a)
+        if isinstance(b, float):
+            b = int(b)
     except TypeError as te:
-        print(te)
+        return te
+    return a + b
