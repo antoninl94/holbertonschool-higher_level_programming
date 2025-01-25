5b2ee0f131bf4c7695f10d3f1dc0d7f6af74c665
antoninl94 <9803@holbertonstudents.com>
2025-01-25 16:13:33 +0100

    4-print_square.py/.txt created

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the 4-print_square module.
+
+>>> print_square(4)
+####
+####
+####
+####
+"""
+
+
+def print_square(size):
+    """
+    This function print a "#" square of size "size".
+    """
+    if size < 0:
+        raise Exception('size must be >= 0')
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
