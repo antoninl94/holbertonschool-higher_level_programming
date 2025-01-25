52e4ae49ef0946b4764503231deb2ecf9a4b3bd3
antoninl94 <9803@holbertonstudents.com>
2025-01-25 14:24:20 +0100

    2-matrix_divided.py/.txt fixed division by zero and pycodestyle

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -21,16 +21,19 @@ def matrix_divided(matrix, div):
     """
     Divide all elements of a matrix and return the result.
     """
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
     for row in matrix:
         if len(row) != len(matrix[0]):
             raise TypeError('Each row of the matrix must have the same size')
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+                raise TypeError('matrix must be a matrix\
+ (list of lists) of integers/floats')
     try:
         result = [[divide(element, div) for element in row] for row in matrix]
         return result
-    except ZeroDivisionError as zde:
-        return zde
+    except ZeroDivisionError as e:
+        return e
     except TypeError as te:
         return te
