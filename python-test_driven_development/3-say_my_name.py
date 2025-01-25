82c25742108af1c56a3a24c69401d2b68fd82377
antoninl94 <9803@holbertonstudents.com>
2025-01-25 15:37:38 +0100

    3-say_my_name.py added documentation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This is the 3-say_my_name module.
+
+>>> say_my_name("John", "Doe")
+My name is John Doe
+"""
+
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print the First name and the Last name preceded by "My name is"
+    """
     if not isinstance(first_name, str):
         raise Exception("first_name must be a string")
     if not isinstance(last_name, str):
