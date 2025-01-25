42fc87314ace83ca5803e09a8ca24897d4817ffc
antoninl94 <9803@holbertonstudents.com>
2025-01-25 17:11:16 +0100

    5-text_indentation.py/.txt created

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This is the 5-text_indentation module.
+
+"""
+
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] == "," or text[i] == "?" or text[i] == ":":
+            print()
+            print()
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
