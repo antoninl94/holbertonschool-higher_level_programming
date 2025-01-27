87eb4209da78eb6e5bd39f63c9e8b5f02859981f
antoninl94 <9803@holbertonstudents.com>
2025-01-27 11:18:12 +0100

    1-main/ 1-square : created

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the '0-square' module.
+"""
+
+
+class Square:
+    """
+    This is the square class.
+    """
+    def __init__(self, size):
+        self.__size = size
