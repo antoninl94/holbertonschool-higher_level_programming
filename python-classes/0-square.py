8565103d1cec38359e4d592a55d58e627fc56d80
antoninl94 <9803@holbertonstudents.com>
2025-01-27 10:40:01 +0100

    README.md/ 0-main.py/ 0-square.py created

---
+++ python-classes/0-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+"""
+This is the '0-square' module.
+"""
+
+
+class square:
+    """
+    This is the square class.
+    """
+    pass
---
+++ python-classes/README.md
@@ -0,0 +1,5 @@
+# Python Classes
+
+## Synopsis
+
+This directory is about the python classes and how they are used in OOP language Python
