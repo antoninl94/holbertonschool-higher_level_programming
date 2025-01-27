a3b698f373d52b99037b168f7888effe84f8f2db
antoninl94 <9803@holbertonstudents.com>
2025-01-27 14:45:02 +0100

    4-square/ 4-main created

---
+++ python-classes/4-square.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is the '0-square' module.
+"""
+
+
+class Square:
+    """
+    This is the square class.
+    """
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    @size.setter
+    def size(self, size):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
