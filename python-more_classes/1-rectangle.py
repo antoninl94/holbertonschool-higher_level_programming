3ce3c5c853d5494163c3126878a4c76302605875
antoninl94 <9803@holbertonstudents.com>
2025-01-28 15:33:49 +0100

    1-rectangle added underscore in def init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
     This is the rectangle class.
     """
     def __init__(self, width=0, height=0):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
