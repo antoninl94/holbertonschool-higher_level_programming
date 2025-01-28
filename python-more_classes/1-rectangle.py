3caf1bdc68485b639199c14c032d59a2bb1d0182
antoninl94 <9803@holbertonstudents.com>
2025-01-28 15:23:00 +0100

    1-rectangle moved conditions

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,16 +9,8 @@ class Rectangle:
     This is the rectangle class.
     """
     def __init__(self, width=0, height=0):
-        if not isinstance(width, int):
-            raise TypeError('width must be an integer')
-        if not isinstance(height, int):
-            raise TypeError('height must be an integer')
-        if width < 0:
-            raise ValueError('width must be >= 0')
-        if height < 0:
-            raise ValueError('height must be >= 0')
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -32,6 +24,10 @@ class Rectangle:
         """
         This is the width setter of the rectangle
         """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -46,4 +42,8 @@ class Rectangle:
         """
         This is the width setter of the rectangle
         """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
         self.__height = value
