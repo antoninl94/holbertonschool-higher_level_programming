00cb4ac861f20eaaf88e66bfe00346c4be8c1506
antoninl94 <9803@holbertonstudents.com>
2025-01-28 17:02:03 +0100

    1-rectangle removed conditions in instances

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,6 +9,14 @@ class Rectangle:
     This is the rectangle class.
     """
     def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError('height must be an integer')
+        if width < 0:
+            raise ValueError('height must be >= 0')
+        if not isinstance(height, int):
+            raise TypeError('width must be an integer')
+        if height < 0:
+            raise ValueError('width must be >= 0')
         self.__width = width
         self.__height = height
 
@@ -24,10 +32,6 @@ class Rectangle:
         """
         This is the width setter of the rectangle
         """
-        if not isinstance(value, int):
-            raise TypeError('width must be an integer')
-        if value < 0:
-            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -42,8 +46,4 @@ class Rectangle:
         """
         This is the width setter of the rectangle
         """
-        if not isinstance(value, int):
-            raise TypeError('height must be an integer')
-        if value < 0:
-            raise ValueError('height must be >= 0')
         self.__height = value
