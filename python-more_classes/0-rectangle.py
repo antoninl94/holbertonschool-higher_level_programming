6646d9e98add9eca8b0a8a0970d32d7d955e3f88
antoninl94 <9803@holbertonstudents.com>
2025-01-28 14:47:05 +0100

    README.md/ 0-rectangle/ 0-main created

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This is the '0-rectangle' module.
+"""
+
+
+class Rectangle:
+    """
+    This is the rectangle class.
+    """
+    def __init__(self):
+        pass
---
+++ python-more_classes/README.md
@@ -0,0 +1,5 @@
+# Python : More Classes
+
+## Synopsis
+
+This directory contain all the task about the classes in Python Language
