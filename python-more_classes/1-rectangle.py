a69e39cecb0fe45293e4b02868dfe746827fe57a
antoninl94 <9803@holbertonstudents.com>
2025-01-28 15:00:53 +0100

    1-rectangle/ 1-main created

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This is the '0-rectangle' module.
+"""
+
+
+class Rectangle:
+    """
+    This is the rectangle class.
+    """
+    def __init__(self, width=0, height=0):
+        if not isinstance(width, int):
+            raise TypeError('width must be an integer')
+        if not isinstance(height, int):
+            raise TypeError('height must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        This is the width getter of the rectangle
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        This is the width setter of the rectangle
+        """
+        self.__width == value
+
+    @property
+    def height(self):
+        """
+        This is the width getter of the rectangle
+        """
+        return self.__height
+
+    @width.setter
+    def height(self, value):
+        """
+        This is the width setter of the rectangle
+        """
+        self.__height == value
