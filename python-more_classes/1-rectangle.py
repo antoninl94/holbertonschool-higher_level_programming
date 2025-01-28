22325cc17d045a53eaabc9a5e70892961b64576d
antoninl94 <9803@holbertonstudents.com>
2025-01-28 15:45:15 +0100

    1-rectangle added condition in def init

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -9,8 +9,16 @@ class Rectangle:
     This is the rectangle class.
     """
     def __init__(self, width=0, height=0):
-        self.__width = width
-        self.__height = height
+        if not isinstance(width, int):
+            raise TypeError('height must be an integer')
+        if width < 0:
+            raise ValueError('height must be >= 0')
+        if not isinstance(height, int):
+            raise TypeError('width must be an integer')
+        if height < 0:
+            raise ValueError('width must be >= 0')
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
