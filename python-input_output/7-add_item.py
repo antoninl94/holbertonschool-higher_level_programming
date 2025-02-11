85363ff3b590002575e7d5fdd1c519656ea9e4d5
antoninl94 <9803@holbertonstudents.com>
2025-02-11 16:21:54 +0100

    7-add_item created

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is the ``7-add_item`` module
+"""
+import sys
+import json
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    items = load_from_json_file(filename)
+except Exception:
+    items = []
+
+items.extend(sys.argv[1:])
+save_to_json_file(items, filename)
