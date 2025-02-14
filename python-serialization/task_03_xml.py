79aaa1e415d22dd238d859e310bbd2b2577d9636
antoninl94 <9803@holbertonstudents.com>
2025-02-14 18:31:24 +0100

    task_03 removed try.except

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -9,27 +9,21 @@ def serialize_to_xml(dictionary, filename):
     """
     This function serialize an object in xml format
     """
-    try:
-        root = ET.Element("data")
-        for key, value in dictionary.items():
-            items = ET.SubElement(root, key)
-            items.text = str(value)
-            tree = ET.ElementTree(root)
-            tree.write(filename)
-    except Exception:
-        return
+    root = ET.Element("data")
+    for key, value in dictionary.items():
+        items = ET.SubElement(root, key)
+        items.text = str(value)
+        tree = ET.ElementTree(root)
+        tree.write(filename)
 
 
 def deserialize_from_xml(filename):
     """
     This function create an object from a xml file
     """
-    try:
-        data = ET.parse(filename)
-        root = data.getroot()
-        dictionnary = {}
-        for child in root:
-            dictionnary[child.tag] = child.text
-        return dictionnary
-    except Exception:
-        return
+    data = ET.parse(filename)
+    root = data.getroot()
+    dictionnary = {}
+    for child in root:
+        dictionnary[child.tag] = child.text
+    return dictionnary
