67e4d463ada127d76de8b4cffe618be758160307
antoninl94 <9803@holbertonstudents.com>
2025-02-19 11:23:45 +0100

    task_03_http_server moved send_response before send_header

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -17,27 +17,27 @@ class BaseHTTPSubclass(http.server.BaseHTTPRequestHandler):
         This function handle GET requests
         """
         if self.path == '/':
+            self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.send_response(200)
             self.wfile.write("Hello, this is a simple API!".encode())
         elif self.path == '/data':
+            self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             resp = {"name": "John", "age": 30, "city": "New York"}
-            self.send_response(200)
             self.wfile.write(json.dumps(resp).encode())
         elif self.path == '/info':
+            self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
             resp = {"version": "1.0", "description":
                     "A simple API built with http.server"}
-            self.send_response(200)
             self.wfile.write(json.dumps(resp).encode())
         elif self.path == '/status':
+            self.send_response(200)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
-            self.send_response(200)
             self.wfile.write("Ok".encode())
         else:
             self.send_error(404, "Endpoint not found")
