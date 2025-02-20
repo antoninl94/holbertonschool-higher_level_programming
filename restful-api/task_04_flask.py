62efcf449f59d489e2039cace7f8e284e8fb18b1
antoninl94 <9803@holbertonstudents.com>
2025-02-20 10:20:01 +0100

    task_04_flask.py created

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This is the ``task_04_flask`` module
+"""
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+users = {}
+
+
+@app.route('/')
+def home():
+    return "<p>Welcome to the Flask API!</p>"
+
+
+@app.route('/data')
+def data():
+    return jsonify(list(users.keys()))
+
+
+@app.route('/status')
+def status():
+    return "<p>OK</p>"
+
+
+@app.route('/users/<username>')
+def userInfo(username):
+    user = users.get(username)
+    if user is not None:
+        return jsonify(user)
+    else:
+        error = {"error": "User not found"}
+        return jsonify(error), 404
+
+
+@app.route('/add_user', methods=['POST'])
+def add_user():
+    """
+    Add a new user
+    """
+    userdata = request.get_json()
+    username = userdata.get('username')
+    if not username:
+        return jsonify({"error":"Username is required"}), 400
+    users[username] = userdata
+    return jsonify({"message": "User added"}), 201
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
