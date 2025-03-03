ff6983f5980709c7fc7f1c1b85f0c934f45fe9d7
antoninl94 <9803@holbertonstudents.com>
2025-03-03 15:56:16 +0100

    5-full_table and 6-list_values created

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- Print the following description of a given database
+-- This is the script that provide to print the description
+SHOW CREATE TABLE first_table;
+
---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- This task provide to list all rows in a table
+-- This is the script to list rows in a table
+SELECT * FROM first_table;
