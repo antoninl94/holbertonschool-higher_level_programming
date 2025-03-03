9a4f118cf4ec6017e5243864d08915dafd2b7425
antoninl94 <9803@holbertonstudents.com>
2025-03-03 14:32:00 +0100

    2-remove_database, 3-list_tables created

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- This task is about deleting a database with a script
+-- The script providing the deletion of a database
+DROP DATABASE IF EXISTS hbtn_0c_0;
---
+++ SQL_introduction/3-list_tables.sql
@@ -0,0 +1,3 @@
+-- This task is about listing all tables in a database
+-- This script is providing to list all the tables in a given database
+SHOW TABLES;
