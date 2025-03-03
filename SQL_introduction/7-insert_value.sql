050e0106505ee30a0372305625c97e5a70afc6cc
antoninl94 <9803@holbertonstudents.com>
2025-03-03 16:12:32 +0100

    7-insert_value, 8-count_89 created

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- This task is about the adding of new rows in a given table
+-- This is the script that provide to add rows in a table
+INSERT INTO first_table
+(id, name)
+VALUES (89, 'Best School');
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- This task is about creating a script to display number of records
+-- This is the script to display number of records
+SELECT COUNT(id) FROM first_table;
