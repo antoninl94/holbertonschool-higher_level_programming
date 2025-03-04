6efb3422526bf6e8aaf27c07b568d0d8e91a9cec
antoninl94 <9803@holbertonstudents.com>
2025-03-04 11:15:02 +0100

    3-force_name.sql created

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,6 @@
+-- This task is about creating a table in a given database
+-- This script create a table in a given database
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT,
+    name VARCHAR(256)
+);
