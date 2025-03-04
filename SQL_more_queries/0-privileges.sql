7471dc36dfec3e216fd3ee902aec7eaad1c2653b
antoninl94 <9803@holbertonstudents.com>
2025-03-04 10:08:55 +0100

    0-privileges.sql hotfix

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- In this task we have to list all privileges of users of the MySQL server
 -- This is the script to list all privileges of users of the MySQL server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
