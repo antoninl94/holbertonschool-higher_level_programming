d2c9836044018cf663efbb1d732e793adc1ef253
antoninl94 <9803@holbertonstudents.com>
2025-03-04 11:25:25 +0100

    5-unique_id.sql hotfix

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- This task is about creating a table in a given database
 -- This script create a table in a given database
 CREATE TABLE IF NOT EXISTS unique_id (
-    id INT DEFAULT UNIQUE 1,
+    id INT UNIQUE DEFAULT 1,
     name VARCHAR(256)
 );
