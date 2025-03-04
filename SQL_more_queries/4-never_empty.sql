bb127fb7323ddbba0180fbe163e154a13be7f5cb
antoninl94 <9803@holbertonstudents.com>
2025-03-04 11:19:51 +0100

    4-never_empty.sql created

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- This task is about creating a table in a given database
+-- This script create a table in a given database
+CREATE TABLE IF NOT EXISTS id_not_null (
+    id INT DEFAULT 1,
+    name VARCHAR(256)
+);
