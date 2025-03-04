82055ba13f39ced5b5887574f28d21feea5dc278
antoninl94 <9803@holbertonstudents.com>
2025-03-04 10:43:17 +0100

    1-create_user.sql created

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- In this task we gonna talk about the creation of a user
+-- This script will create a user with all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
