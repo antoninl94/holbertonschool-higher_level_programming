78a5ef78e06f711df5736898854efd3e5a2a21e5
antoninl94 <9803@holbertonstudents.com>
2025-03-04 11:35:04 +0100

    6-states.sql added auto_increment

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 -- This script create a table in a given database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(256)
 );
