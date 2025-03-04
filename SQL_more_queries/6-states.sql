9024dd5cad667b0da73ea9e747cfdf55e43a74c0
antoninl94 <9803@holbertonstudents.com>
2025-03-04 11:33:16 +0100

    6-states.sql created

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- This task is about creating a table in a given database
+-- This script create a table in a given database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id INT PRIMARY KEY NOT NULL,
+    name VARCHAR(256)
+);
