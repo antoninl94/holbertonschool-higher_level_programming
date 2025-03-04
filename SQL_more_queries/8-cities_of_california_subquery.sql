6e3764b7edc5406748e3ea5565da72656d26067f
antoninl94 <9803@holbertonstudents.com>
2025-03-04 13:57:28 +0100

    8-cities_of_california_subquery.sql hotfix

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,5 @@
 -- In this task we have to display all the cities contained in the database hbtn_0d_usa
 -- This script display all the cities in a given table
-SELECT id, name FROM states
-WHERE name = 'California'
-ORDER BY id ASC;
+SELECT id, name FROM cities
+WHERE state_id IN (SELECT id FROM states)
+ORDER BY cities.id ASC;
