288269c551df224b16f18deb10f18f42f16f3e70
antoninl94 <9803@holbertonstudents.com>
2025-03-05 10:22:44 +0100

    13-count_shows_by_genre.sql too hotfix

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- In this task we will list all the genres that have at least one show+ linked
 -- This is the script that will list all the shows
-SELECT tv_genres.name, count(tv_show_genres.show_id) AS number_of_shows
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id) AS number_of_shows
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
