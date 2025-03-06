786fc4a34c65af6331d0a835732fbbffad0fc8ff
antoninl94 <9803@holbertonstudents.com>
2025-03-06 14:49:45 +0100

    1-filter_states.py replaced % by *

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                            passwd=sys.argv[2],
                            db=sys.argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N*' ORDER BY id")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
     states = cur.fetchall()
     for state in states:
         print(state)
