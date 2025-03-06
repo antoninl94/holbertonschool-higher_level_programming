274865e58a9cbdb233c35cdbbc34b2c8d2de76b1
antoninl94 <9803@holbertonstudents.com>
2025-03-06 14:10:00 +0100

    1-filter_states.py created

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This script list all states from a database
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(host="localhost",
+                           user=sys.argv[1],
+                           passwd=sys.argv[2],
+                           db=sys.argv[3])
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    conn.close()
