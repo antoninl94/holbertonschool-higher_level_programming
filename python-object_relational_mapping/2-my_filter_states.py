16f1280ac480df858b1f37d0dcded81996adc62a
antoninl94 <9803@holbertonstudents.com>
2025-03-06 18:26:16 +0100

    2-my_filter_states.py removed ORDER BY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -17,8 +17,7 @@ if __name__ == "__main__":
                            port=3306)
     cur = conn.cursor()
     state_name = sys.argv[4]
-    query = "SELECT * FROM states WHERE name='{}'\
-        ORDER BY id".format(state_name)
+    query = "SELECT * FROM states WHERE name='{}".format(state_name)
     cur.execute(query)
     states = cur.fetchall()
     for state in states:
