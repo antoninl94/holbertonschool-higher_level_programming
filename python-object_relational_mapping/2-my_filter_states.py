97c59781a12b9a66e64bf7e7ac84175fda72d64f
antoninl94 <9803@holbertonstudents.com>
2025-03-06 18:03:49 +0100

    2-my_filter_states.py added length condition

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,6 +6,10 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
     conn = MySQLdb.connect(host="localhost",
                            user=sys.argv[1],
                            password=sys.argv[2],
