153ead56523d6bddaca0a39131a13b1ebda64793
antoninl94 <9803@holbertonstudents.com>
2025-03-06 12:05:58 +0100

    0-select_states.py removed connect arg

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,7 @@ This script list all states from a database
 import MySQLdb
 
 if __name__ == "__main__":
-    conn = MySQLdb.connect(host="localhost",
-                           user="root",
+    conn = MySQLdb.connect(user="root",
                            passwd="root",
                            db="hbtn_0e_0_usa")
     cur = conn.cursor()
