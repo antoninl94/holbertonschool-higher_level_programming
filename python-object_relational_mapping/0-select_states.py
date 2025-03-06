4606f897ec71ad65c2810c0beb01ada34aef1d25
antoninl94 <9803@holbertonstudents.com>
2025-03-06 11:44:51 +0100

    0-select_states.py added cur.close and pycodestyle hotfix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,13 @@ import MySQLdb
 
 if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost",
-                        user="root",
-                        passwd="12345",
-                        db="hbtn_0e_0_usa")
+                           user="root",
+                           passwd="12345",
+                           db="hbtn_0e_0_usa")
     cur = conn.cursor()
     cur.execute("SELECT * FROM hbtn_0e_0_usa.states")
     states = cur.fetchall()
     for state in states:
         print(state)
+    cur.close()
     conn.close()
