e0daa60b5dc2b66f6b2d406cb6e640cd8d44d9b8
antoninl94 <9803@holbertonstudents.com>
2025-03-06 18:27:06 +0100

    2-my_filter_states.py added ORDER BY

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -6,10 +6,6 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-
-    if len(sys.argv) != 5:
-        sys.exit(1)
-
     conn = MySQLdb.connect(host="localhost",
                            user=sys.argv[1],
                            password=sys.argv[2],
@@ -17,7 +13,8 @@ if __name__ == "__main__":
                            port=3306)
     cur = conn.cursor()
     state_name = sys.argv[4]
-    query = "SELECT * FROM states WHERE name='{}".format(state_name)
+    query = "SELECT * FROM states WHERE name='{}'\
+        ORDER BY id ASC;".format(state_name)
     cur.execute(query)
     states = cur.fetchall()
     for state in states:
