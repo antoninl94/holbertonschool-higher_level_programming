b2cae4efed2538eb271e8d444fdec7028e33cd38
antoninl94 <9803@holbertonstudents.com>
2025-03-07 22:24:26 +0100

    7-model_state_fetch_all.py if __name__=="__main__" added

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -8,18 +8,20 @@ from model_state import Base, State
 import sys
 
 
-engine = sa.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    sys.argv[1],
-    sys.argv[2],
-    sys.argv[3]),
-    pool_pre_ping=True
-    )
 
-conn = engine.connect()
-Session = sessionmaker(bind=conn)
-session = Session()
+if __name__ == "__main__":
+    engine = sa.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1],
+        sys.argv[2],
+        sys.argv[3]),
+        pool_pre_ping=True
+        )
 
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
+    conn = engine.connect()
+    Session = sessionmaker(bind=conn)
+    session = Session()
 
-session.close()
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+
+    session.close()
