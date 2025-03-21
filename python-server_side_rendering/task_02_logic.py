8d7c6c91ee5d3ce46af37a70e4bde0c8c601c801
antoninl94 <9803@holbertonstudents.com>
2025-03-21 19:36:36 +0100

    task_02_logic.py pycodestyle hotfix

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
 """
-In this task we will build a basic Flask application that serves a web page using a Jinja template
+In this task we will build a basic Flask application
+that serves a web page using a Jinja template
 """
 from flask import Flask, render_template
 import json
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
 
 @app.route('/items')
 def items():
@@ -26,6 +31,7 @@ def items():
         item_list = data['items']
         print(item_list)
     return render_template('items.html', items=item_list)
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
