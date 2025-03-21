dab830251363d059305d18763ae3b4f8e0068925
antoninl94 <9803@holbertonstudents.com>
2025-03-21 16:34:19 +0100

    task_01_jinja.py templates created

---
+++ python-server_side_rendering/task_01_jinja.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+In this task we will build a basic Flask application that serves a web page using a Jinja template
+"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=3000)
